359d00f53f670994c3fb021cc09a252d770a6303
ViniciusQuirino <viniciusquirino13@hotmail.com>
2023-04-25 21:37:26 -0300

    create db

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,14 +1,27 @@
-import 'reflect-metadata';
-import 'dotenv/config';
-import path from 'path';
-import { DataSource, DataSourceOptions } from 'typeorm';
+import "reflect-metadata";
+import "dotenv/config";
+import path from "path";
+import { DataSource, DataSourceOptions } from "typeorm";
 
 const setDataSourceConfig = (): DataSourceOptions => {
-  const entitiesPath = path.join(__dirname, './entities/**.{js,ts}');
-  const migrationsPath = path.join(__dirname, './migrations/**.{js,ts}');
+  const entitiesPath = path.join(__dirname, "./entities/**.{js,ts}");
+  const migrationsPath = path.join(__dirname, "./migrations/**.{js,ts}");
 
+  const nodeEnv: string | undefined = process.env.NODE_ENV;
+  const dbUrl: string | undefined = process.env.DATABASE_URL;
+
+  if (nodeEnv === "production") {
+    return {
+      type: "postgres",
+      url: dbUrl,
+      synchronize: false,
+      logging: true,
+      migrations: [migrationsPath],
+      entities: [entitiesPath],
+    };
+  }
   return {
-    type: 'postgres',
+    type: "postgres",
     host: process.env.PGHOST,
     username: process.env.PGUSER,
     password: process.env.PGPASSWORD,
