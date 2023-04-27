dccd006a0b02a4bf1526faf41d77ebfe7379c545
ViniciusQuirino <viniciusquirino13@hotmail.com>
2023-04-27 10:56:23 -0300

    create api webhook

--- src/api.ts
+++
@@ -1,8 +0,0 @@
-// import axios from "axios";
-
-// const apiFood = axios.create({
-//   baseURL: "https://app.foodydelivery.com/rest/1.2/orders",
-//   timeout: 15000,
-// });
-
-// export default apiFood;
--- src/services/entregas/createEntregas.service.ts
+++ src/services/entregas/createEntregas.service.ts
@@ -3,71 +3,67 @@ import AppDataSource from "../../data-source";
 import { Entregas } from "../../entities/entregas.entites";
 
 const createEntregasService = async (body: any) => {
-  if (body.oi == "oi") {
-    console.log(body);
-  } else if (body.oi == null) {
-    const data = {
-      id: "301",
-      status: "open",
-      notes: "Entrega paga",
-      deliveryPoint: {
-        address: "avn pedro ometo 1401 barra bonita sp",
+  const data = {
+    id: "301",
+    status: "open",
+    notes: "Entrega paga",
+    deliveryPoint: {
+      address: "avn pedro ometo 1401 barra bonita sp",
+    },
+  };
+
+  const response = await fetch(
+    "https://app.foodydelivery.com/rest/1.2/orders",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "326b3f2b4c9f4bcdb48363c0d023260c",
       },
-    };
+      body: JSON.stringify(data),
+    }
+  )
+    .then((res) => res.json())
+    .then((res) => res)
+    .catch((err) => console.log(err));
 
-    const response = await fetch(
-      "https://app.foodydelivery.com/rest/1.2/orders",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: "326b3f2b4c9f4bcdb48363c0d023260c",
-        },
-        body: JSON.stringify(data),
-      }
-    )
-      .then((res) => res.json())
-      .then((res) => res)
-      .catch((err) => console.log(err));
+  const dados = {
+    telefone: "5514998536591",
+    entregaidfood: response.uid,
+  };
 
-    const dados = {
-      telefone: "5514998536591",
-      entregaidfood: response.uid,
-    };
+  const webHook = await fetch(
+    "https://webhook-api-sos.onrender.com/webhook/create",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(dados),
+    }
+  )
+    .then((res) => res.json())
+    .then((res) => res)
+    .catch((err) => console.log(err));
 
-    const webHook = await fetch(
-      "https://webhook-api-sos.onrender.com/webhook/create",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(dados),
-      }
-    )
-      .then((res) => res.json())
-      .then((res) => res)
-      .catch((err) => console.log(err));
+  console.log(webHook);
 
-    console.log(webHook);
+  const doido = {
+    id: response.uui,
+    tokencoleta: "326b3f2b4c9f4bcdb48363c0d023260c",
+    entrega: response.deliveryPoint.address,
+    obs: "salve salve",
+    formadepagamento: "pix",
+    telefone: "5514998536591@c.us",
+  };
 
-    const doido = {
-      id: response.uui,
-      tokencoleta: "326b3f2b4c9f4bcdb48363c0d023260c",
-      entrega: response.deliveryPoint.address,
-      obs: "salve salve",
-      formadepagamento: "pix",
-      telefone: "5514998536591@c.us",
-    };
+  const entregasRepositorio = AppDataSource.getRepository(Entregas);
 
-    const entregasRepositorio = AppDataSource.getRepository(Entregas);
+  const entregas = entregasRepositorio.create(doido);
 
-    const entregas = entregasRepositorio.create(doido);
+  await entregasRepositorio.save(entregas);
 
-    await entregasRepositorio.save(entregas);
-
-    return entregas;
-  }
+  return entregas;
 };
 
 export default createEntregasService;
