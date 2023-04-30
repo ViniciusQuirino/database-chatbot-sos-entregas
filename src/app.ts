81814a4757ebe4bb7b202c61595c877f3ddbfbd4
ViniciusQuirino <viniciusquirino13@hotmail.com>
2023-04-30 14:49:47 -0300

    db

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import cors from "cors";
 import clientesRoutes from "./routes/clientes.routes";
 import etapasRoutes from "./routes/etapas.routes";
 import entregasRoutes from "./routes/entregas.routes";
+import { handleErrorMiddleware } from "./errors/appError";
 
 const app = express();
 
@@ -13,5 +14,6 @@ app.use(cors());
 app.use("/clientes", clientesRoutes);
 app.use("/etapas", etapasRoutes);
 app.use("/entregas", entregasRoutes);
+app.use(handleErrorMiddleware);
 
 export default app;
--- src/services/clientes/createCliente.service.ts
+++ src/services/clientes/createCliente.service.ts
@@ -1,9 +1,18 @@
 import AppDataSource from "../../data-source";
 import { Clientes } from "../../entities/clientes.entities";
+import { AppError } from "../../errors/appError";
 import { iClientRequest } from "../../interface";
 
 const createClientService = async (data: iClientRequest) => {
   const clienteRepositorio = AppDataSource.getRepository(Clientes);
+
+  const encontrarCodigo: Clientes = await clienteRepositorio.findOneBy({
+    codigo: data.codigo,
+  });
+
+  if (encontrarCodigo) {
+    throw new AppError("Já existe um cliente cadastrado com esse código.", 400);
+  }
 
   const cliente = clienteRepositorio.create(data);
 
