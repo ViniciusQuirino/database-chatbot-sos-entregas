02439b218d9b0559a0d3d56d9bad62a22d2e3d34
ViniciusQuirino <viniciusquirino13@hotmail.com>
2023-05-06 09:33:02 -0300

    db chatbot

--- src/services/entregas/listEntrega.service.ts
+++ src/services/entregas/listEntrega.service.ts
@@ -3,18 +3,20 @@ import AppDataSource from "../../data-source";
 import { Entregas } from "../../entities/entregas.entites";
 
 const listEntregasServices = async (params) => {
-  const entregaRepositorio: Repository<Entregas> =
-    AppDataSource.getRepository(Entregas);
+  if (params) {
+    const entregaRepositorio: Repository<Entregas> =
+      AppDataSource.getRepository(Entregas);
 
-  let inicio = params.slice(0, 2);
-  let final = params.slice(params.length - 2);
+    let inicio = params.slice(0, 2);
+    let final = params.slice(params.length - 2);
 
-  const entregas: Entregas[] = await entregaRepositorio
-    .createQueryBuilder("entregas")
-    .where("entregas.data = :data", { data: `${inicio}/${final}` })
-    .getMany();
+    const entregas: Entregas[] = await entregaRepositorio
+      .createQueryBuilder("entregas")
+      .where("entregas.data = :data", { data: `${inicio}/${final}` })
+      .getMany();
 
-  return entregas;
+    return entregas;
+  }
 };
 
 export default listEntregasServices;
